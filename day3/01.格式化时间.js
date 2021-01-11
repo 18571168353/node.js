278fba21930c198f4d1479688e38af5deb634aa0
2730747608@qq.com <2730747608@qq.com>
2021-01-11 09:15:06 +0800

    创建day5

--- day3/01.格式化时间.js
+++
@@ -1,3 +0,0 @@
-const moment = require('moment')
-const time= moment().format('YYYY-MM-DD hh:mm:ss')
-console.log(time);
--- day3/02.test.js
+++
@@ -1,8 +0,0 @@
-const test = require('./gk-tools')
-const m = test.dateFormat(new Date())
-console.log(m)
-const htmlStr = '<h1 title="abc">这是h1标签<span>123&nbsp;</span></h1>'
-const str=test.htmlEscape(htmlStr)
-console.log(str);
-const str1=test.htmlUnEscape(str)
-console.log(str1);
